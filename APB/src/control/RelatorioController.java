598c38364521a12f390015fe213e61574c783c35
thyago241 <thiagoribeironiquel@hotmail.com>
2014-08-25 20:32:02 -0300

    Comentário da classe Relarório Controller
    Comentários finais da classe RelatórioController

--- APB/src/control/RelatorioController.java
+++ APB/src/control/RelatorioController.java
@@ -8,7 +8,15 @@ import java.sql.SQLException;
 import dao.RelatorioDAO;
 import model.Relatorio;
 
-// Classe criada como controle do Relatorio
+/*
+ *  Classe criada como controle do Relatorio
+ *  Ele e responssavel por fazer o tratamento de excecao das variaveis:
+ *  pesquisarPorData; pesquisarPorDataEBarbeiro
+ *  pesquisarPorDataEServico; pesquisarPorBarbeiro
+ *  pesquisarPorBarbeiroEServico; pesquisarPorServico;
+ *  pesquisarPorDataBarbeiroEServico; 
+ *  Variaveis que vao entrar com valores para as pesquisas
+ */
 public class RelatorioController {
 
 
@@ -16,30 +24,37 @@ private static RelatorioController instance;
 
 	public RelatorioController() {}
 	
+	// Metodo que cede o acesso aos relatorios cadastrados e da a opcao de que sejam pesquisados por data
 	public ResultSet pesquisarPorData(Relatorio relatorio) throws SQLException {
 		return RelatorioDAO.getInstance().pesquisarPorData(relatorio);
 	}
 	
+	// Metodo que cede o acesso aos relatorios cadastrados e da a opcao de que sejam pesquisados por data e por barbeiro
 	public ResultSet pesquisarPorDataEBarbeiro(Relatorio relatorio) throws SQLException {	
 		return RelatorioDAO.getInstance().pesquisarPorDataEBarbeiro(relatorio);
 	}
 	
+	// Metodo que cede o acesso aos relatorios cadastrados e da a opcao de que sejam pesquisados por data e servico
 	public ResultSet pesquisarPorDataEServico(Relatorio relatorio) throws SQLException {	
 		return RelatorioDAO.getInstance().pesquisarPorDataEServico(relatorio);
 	}
 	
+	// Metodo que cede o acesso aos relatorios cadastrados e da a opcao de que sejam pesquisados por barbeiro
 	public ResultSet pesquisarPorBarbeiro(Relatorio relatorio) throws SQLException {	
 		return RelatorioDAO.getInstance().pesquisarPorBarbeiro(relatorio);
 	}
 	
+	// Metodo que cede o acesso aos relatorios cadastrados e da a opcao de que sejam pesquisados por barbeiro e servico
 	public ResultSet pesquisarPorBarbeiroEServico(Relatorio relatorio) throws SQLException {	
 		return RelatorioDAO.getInstance().pesquisarPorBarbeiroEServico(relatorio);
 	}
 	
+	// Metodo que cede o acesso aos relatorios cadastrados e da a opcao de que sejam pesquisados por servico
 	public ResultSet pesquisarPorServico(Relatorio relatorio) throws SQLException {	
 		return RelatorioDAO.getInstance().pesquisarPorServico(relatorio);
 	}
 	
+	// Metodo que cede o acesso aos relatorios cadastrados e da a opcao de que sejam pesquisados por bagageiro e servico
 	public ResultSet pesquisarPorDataBarbeiroEServico(Relatorio relatorio) throws SQLException {		
 		return RelatorioDAO.getInstance().pesquisarPorDataBarbeiroEServico(relatorio);
 	}
