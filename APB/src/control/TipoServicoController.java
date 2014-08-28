525f51982fbd655a9d507d2c37c2c13d8b66c6ff
Leonardo <leoccambraia@yahoo.com.br>
2014-08-28 16:35:09 -0300

    Comentário da classe TipoServicoController

--- APB/src/control/TipoServicoController.java
+++ APB/src/control/TipoServicoController.java
@@ -1,15 +1,20 @@
+// Pacote ao qual pertence a classe
 package control;
 
+// Importando dados
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import dao.TipoServicoDAO;
 import model.TipoServico;
+// Fim da importação dos dados
 
+// Início da classe
 public class TipoServicoController {
 
 	private static TipoServicoController instance;
 
+// Método de inserir o tipo de serviço
 	public boolean inserir(TipoServico tipoServico) throws SQLException {
 		if (tipoServico == null) {
 			return false;
@@ -18,7 +23,9 @@ public class TipoServicoController {
 			return true;
 		}
 	}
+// Fim do método
 
+// Método de alterar o tipo de serviço
 	public boolean alterar(String nome,TipoServico tipoServico) throws SQLException {
 		if (tipoServico == null) {
 			return false;
@@ -28,7 +35,9 @@ public class TipoServicoController {
 			return true;
 		}
 	}
+// Fim do método
 
+// Método de excluir o tipo de serviço
 	public boolean excluir(TipoServico tipoServico) throws SQLException {
 
 		if (tipoServico == null) {
@@ -38,22 +47,31 @@ public class TipoServicoController {
 			return true;
 		}
 	}
+// Fim do método
 
 	private TipoServicoController() {
 	}
 
+// Instanciamento
 	public static TipoServicoController getInstance() {
 		if (instance == null)
 			instance = new TipoServicoController();
 		return instance;
 	}
-	
+// Fim do método
+
+// Método que mostra os tipos de serviços cadastrados
 	public ResultSet mostrarTipoServicoCadastrados(TipoServico servico) throws SQLException {
 		return TipoServicoDAO.getInstance().mostrarTipoServicoCadastrados(servico);
 	}
-	
+// Fim do método
+
+// Método que possibilita a pesquisa por nome
 	public ResultSet pesquisarPorNome(TipoServico servico) throws SQLException {
 		return TipoServicoDAO.getInstance().pesquisarPorNome(servico);
 	}
+// Fim do método
 
 }
+
+// Fim da classe
