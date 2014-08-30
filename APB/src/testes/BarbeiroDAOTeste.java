7ddf500b582120c22b2d6654e6ed75a22f74462b
Italo Paiva <italosensei@hotmail.com>
2014-08-30 14:57:53 -0300

    Comentários na classe BarbeiroDAOTeste
    Classe 'BarbeiroDAOTeste' se encontra no pacote 'testes'.

--- APB/src/testes/BarbeiroDAOTeste.java
+++ APB/src/testes/BarbeiroDAOTeste.java
@@ -1,6 +1,10 @@
 package testes;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -19,7 +23,12 @@ public class BarbeiroDAOTeste {
 
 	Barbeiro barbeiro = new Barbeiro();
 	Barbeiro barbeiro2 = new Barbeiro();
+	
 	@Before
+	/*
+	 * Inicializa dois barbeiros como nomes diferentes e dados iguais.
+	 * Captura exceções da inclusão dos dados.
+	 */
 	public void setUp() {
 		try {
 			barbeiro.setNome("Alessandro");
@@ -47,11 +56,22 @@ public class BarbeiroDAOTeste {
 	BarbeiroDAO barbeiroDAO = BarbeiroDAO.getInstance();
 	
 	@Test
+	/* 
+	 * Define uma assertiva que é disparada se o retorno do método 'getInstance()' 
+	 * não for igual à instância declarada logo acima (barbeiroDAO).
+	 */
 	public void getInstanceDeBarbeiroDAODeveRetonarInstanciaCorrente() {	
 		assertEquals(BarbeiroDAO.getInstance(), barbeiroDAO);
 	}
 
 	@Test
+	/*
+	 * Define uma assertiva que é disparada se a inclusão não foi feita
+	 *   com sucesso (retorno do método 'incluir()').
+	 * Define outra assertiva que checa se o nome que foi cadastrado no banco de dados é compatível
+	 *	 com o que foi realmente pedido para se cadastrar.
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void inserirDeBarbeiroDAODeveCadastrarUmBarbeiro() {
 		try {
 			assertTrue(barbeiroDAO.incluir(barbeiro));
@@ -67,8 +87,15 @@ public class BarbeiroDAOTeste {
 		}
 		
 	}
-
+	
+	// Define um erro que é esperado
 	@Test (expected = AssertionError.class)
+	/*
+	 * Define uma assertiva que é disparada se a exclusão de um barbeiro não foi feita
+	 * 	 com sucesso (retorno do método 'excluir()').
+	 * Testa se o método 'fail()' funcionou (erro esperado).
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void excluirDeBarbeiroDAODeveEnviarUmBarbeiro() {
 		try {
 			assertTrue(barbeiroDAO.excluir(barbeiro));
@@ -85,6 +112,13 @@ public class BarbeiroDAOTeste {
 	}
 	
 	@Test
+	/*
+	 * Define uma assertiva que é disparada se a alteração não foi feita
+	 *   com sucesso (retorno do método 'alterar()').
+	 * Define outra assertiva que testa se o nome foi realmente alterado, varrendo todas as linhas
+	 * 	 da coluna 'nome' procurando pelo nome 'Alessandro'.
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void alterarDeBarbeiroDaoDeveAlterarUmBarbeiro() {
 		try {
 			assertTrue(barbeiroDAO.alterar(barbeiro.getNome(), barbeiro, barbeiro2));
@@ -106,6 +140,10 @@ public class BarbeiroDAOTeste {
 	}
 	
 	@Test
+	/*
+	 * Testa se ocorre a inclusão de um barbeiro com passagem de argumento nulo.
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void inserirDeBarbeiroDAOPassandoUmBarbeiroNulo() {
 		try {
 			assertFalse(barbeiroDAO.incluir(null));
@@ -115,6 +153,10 @@ public class BarbeiroDAOTeste {
 	}
 	
 	@Test
+	/*
+	 * Testa se ocorre a exclusão de um barbeiro com passagem de argumento nulo.
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void excluirDeBarbeiroDAOPassandoUmBarbeiroNulo() {
 		try {
 			assertFalse(barbeiroDAO.excluir(null));
@@ -124,6 +166,10 @@ public class BarbeiroDAOTeste {
 	}
 	
 	@Test
+	/*
+	 * Testa se ocorre a alteração de um barbeiro com passagem de argumentos nulos.
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void alterarDeBarbeiroDaoPassandoUmBarbeiroNulo() {
 		try {
 			assertFalse(barbeiroDAO.alterar(barbeiro.getNome(), null, null));
@@ -133,6 +179,10 @@ public class BarbeiroDAOTeste {
 	}
 	
 	@Test
+	/*
+	 * Testa se ocorre a inclusão de um barbeiro com passagem do argumento 'barbeiro_alterado' nulo.
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void alterarDeBarbeiroDaoPassandoUmBarbeiroAlteradoNulo() {
 		try {
 			assertFalse(barbeiroDAO.alterar(barbeiro.getNome(), null, barbeiro));
@@ -142,6 +192,10 @@ public class BarbeiroDAOTeste {
 	}
 	
 	@Test
+	/*
+	 * Define uma assertiva que é disparada se há algum nome nulo no banco de dados. 
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void pesquisarDeBarbeiroDAODeveMostrarUmBarbeiro() {
 		try {
 			ResultSet rs = barbeiroDAO.pesquisar();
@@ -156,6 +210,10 @@ public class BarbeiroDAOTeste {
 	}
 	
 	@Test
+	/*
+	 * Define uma assertiva que é disparada se há algum nome nulo no banco de dados. 
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void mostrarBarbeirosCadastradosDeBarbeiroDAODeveMostrarBarbeiros() {
 		try {
 			ResultSet rs = barbeiroDAO.mostrarBarbeirosCadastrados(barbeiro);
@@ -170,6 +228,10 @@ public class BarbeiroDAOTeste {
 	}
 	
 	@Test
+	/*
+	 * Define uma assertiva que é disparada se o nome do barbeiro cadastrado no banco de dados for nulo. 
+	 * Captura exceções SQL que possam ocorrer.
+	 */
 	public void pesquisarPorNomeDeBarbeiroDAODeveMostrarBarbeiros() {
 		try {
 			ResultSet rs = barbeiroDAO.pesquisarPorNome(barbeiro);
