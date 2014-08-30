a2b7155391fff36ef2d0dc49206dc7d68a7dcd6b
Italo Paiva <italosensei@hotmail.com>
2014-08-30 15:17:51 -0300

    Comentários na classe ReciboException
    Classe 'ReciboException' se encontra no pacote 'exception '.

--- APB/src/exception/ReciboException.java
+++ APB/src/exception/ReciboException.java
@@ -3,10 +3,12 @@ package exception;
 @SuppressWarnings("serial")
 public class ReciboException extends Exception {
 
+	// Construtor padrão que referencia o construtor padrão da superclasse
 	public ReciboException() {
 		super();
 	}
-
+	
+	// Construtor que referencia o construtor da superclasse - Adiciona uma mensagem de exceção
 	public ReciboException(String message) {
 		super(message);
 	}
