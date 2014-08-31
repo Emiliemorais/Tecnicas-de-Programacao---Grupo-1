b22bbd33067870486b79898d83717f7c2669b5ef
Leonardo <leoccambraia@yahoo.com.br>
2014-08-31 20:31:15 -0300

    Comentários em uma classe
    Classe:
    - ServicoPrestadoControllerTeste.java"

--- APB/src/testes/ServicoPrestadoControllerTeste.java
+++ APB/src/testes/ServicoPrestadoControllerTeste.java
@@ -1,5 +1,7 @@
+// Pacote ao qual pertence a classe
 package testes;
 
+// Importações
 import static org.junit.Assert.*;
 
 import java.sql.ResultSet;
@@ -13,20 +15,23 @@ import org.junit.Test;
 
 import control.ServicoPrestadoController;
 import exception.ServicoException;
+// Fim das importações
 
+// Início da classe
 public class ServicoPrestadoControllerTeste {
 	ServicoPrestado servico = new ServicoPrestado();
 	ServicoPrestadoController servicoController = ServicoPrestadoController.getInstance();
 
+// Tratamento de exceções
 	@Before
 	public void setUp() throws ServicoException, ParseException {
 		servico.setNomeServico("Corte");
 		servico.setNomeBarbeiro("Joao");
 		servico.setPreco("125,23");
 		servico.setData("20/12/2013");
-
 	}
-	
+
+// Testes
 	@Test
 	public void getInstanceDeServicoPrestadoControllerDeveRetornarInstanciaCorrente() {
 		assertEquals(ServicoPrestadoController.getInstance(), servicoController);
@@ -67,10 +72,13 @@ public class ServicoPrestadoControllerTeste {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void mostrarServicoPrestadoDeServicoPrestadoControllerDeveMostrarUmServico() throws SQLException {
 		ResultSet rs = servicoController.mostrarServicosPrestadosCadastrados(servico);
 		while(rs.next());
 	}
+// Fim dos testes
+
 }
+// Fim da classe
