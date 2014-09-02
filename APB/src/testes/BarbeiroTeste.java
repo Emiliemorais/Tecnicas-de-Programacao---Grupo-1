502f4a49d0e0687f486a146492154f45d442ac5e
Emiliemorais <emilie.mo@hotmail.com>
2014-09-02 08:55:09 -0300

    Aplicaçao da folha de estilo

--- APB/src/testes/BarbeiroTeste.java
+++ APB/src/testes/BarbeiroTeste.java
@@ -12,7 +12,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-public class BarbeiroTeste {
+public class BarbeiroTeste 
+{
 
 	Barbeiro barbeiro;
 	
@@ -21,17 +22,23 @@ public class BarbeiroTeste {
 	 * para realização do teste
 	*/
 	@Before
-	public void setUp() {
-		try {
+	public void setUp()
+	{
+		try 
+		{
 			barbeiro =  new Barbeiro();
 			barbeiro.setNome("Alessandro");
 			barbeiro.setRg("418757896");
 			barbeiro.setTelefone("3389-9085");
 			barbeiro.setCpf("02919594150");
 			barbeiro.setCadeira("10");
-		} catch (NullPointerException e) {
+		} 
+		catch (NullPointerException e) 
+		{
 			e.printStackTrace();
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 	}
@@ -42,10 +49,14 @@ public class BarbeiroTeste {
 	 * do lançamento da exceção
 	*/
 	@Test (expected = NullPointerException.class)
-	public void barbeiroNaoPodePossuirNomeNuloPassandoPeloSetter() {
-		try {
+	public void barbeiroNaoPodePossuirNomeNuloPassandoPeloSetter ()
+	{
+		try 
+		{
 			barbeiro.setNome(null);
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 
@@ -56,10 +67,14 @@ public class BarbeiroTeste {
 	 * do lançamento da exceção
 	*/
 	@Test (expected = NullPointerException.class)
-	public void barbeiroNaoPodePossuirCPFNuloPassandoPeloSetter() {
-		try {
+	public void barbeiroNaoPodePossuirCPFNuloPassandoPeloSetter () 
+	{
+		try 
+		{
 			barbeiro.setCpf(null);
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 
@@ -70,10 +85,14 @@ public class BarbeiroTeste {
 	 * do lançamento da exceção
 	*/
 	@Test (expected = NullPointerException.class)
-	public void barbeiroNaoPodePossuirRGNuloPassandoPeloSetter() {
-		try {
+	public void barbeiroNaoPodePossuirRGNuloPassandoPeloSetter ()
+	{
+		try
+		{
 			barbeiro.setRg(null);
-		} catch (BarbeiroException e) {
+		}
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 
@@ -84,10 +103,14 @@ public class BarbeiroTeste {
 	 * do lançamento da exceção
 	*/
 	@Test (expected = NullPointerException.class)
-	public void barbeiroNaoPodePossuirTelefoneNuloPassandoPeloSetter() {
-		try {
+	public void barbeiroNaoPodePossuirTelefoneNuloPassandoPeloSetter () 
+	{
+		try
+		{
 			barbeiro.setTelefone(null);
-		} catch (BarbeiroException e) {
+		}
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 
@@ -98,10 +121,14 @@ public class BarbeiroTeste {
 	 * do lançamento da exceção
 	*/	
 	@Test (expected = NullPointerException.class)
-	public void barbeiroNaoPodePossuirCadeiraNuloPassandoPeloSetter() {
-		try {
+	public void barbeiroNaoPodePossuirCadeiraNuloPassandoPeloSetter ()
+	{
+		try 
+		{
 			barbeiro.setCadeira(null);
-		} catch (BarbeiroException e) {
+		}
+		catch (BarbeiroException e)
+		{
 			e.printStackTrace();
 		}
 
@@ -112,10 +139,13 @@ public class BarbeiroTeste {
 	 * realização do teste do lançamento da exceção
 	*/
 	@Test (expected = IllegalArgumentException.class)
-	public void contrutorDeBarbeiroNaoPodePassarComNomeNulo() {
-		try {
+	public void contrutorDeBarbeiroNaoPodePassarComNomeNulo () 
+	{
+		try 
+		{
 			new Barbeiro(null, "493.751.185-84", "2258256", "3389-9085", "10");
-		} catch (BarbeiroException e) {
+		} catch (BarbeiroException e)
+		{
 			e.printStackTrace();
 		}
 
@@ -126,10 +156,14 @@ public class BarbeiroTeste {
 	 * realização do teste do lançamento da exceção
 	*/	
 	@Test (expected = IllegalArgumentException.class)
-	public void contrutorDeBarbeiroNaoPodePassarComCpfNulo() {
-		try {
+	public void contrutorDeBarbeiroNaoPodePassarComCpfNulo ()
+	{
+		try 
+		{
 			new Barbeiro("Alessandro", null, "2258256", "3389-9085", "10");
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 
@@ -140,10 +174,14 @@ public class BarbeiroTeste {
 	 * realização do teste do lançamento da exceção
 	*/
 	@Test (expected = IllegalArgumentException.class)
-	public void contrutorDeBarbeiroNaoPodePassarComRgNulo() {
-		try {
+	public void contrutorDeBarbeiroNaoPodePassarComRgNulo () 
+	{
+		try
+		{
 			new Barbeiro("Alessandro", "493.751.185-84", null, "3389-9085", "10");
-		} catch (BarbeiroException e) {
+		}
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 
@@ -154,10 +192,14 @@ public class BarbeiroTeste {
 	 * realização do teste do lançamento da exceção
 	*/	
 	@Test(expected = IllegalArgumentException.class)
-	public void contrutorDeBarbeiroNaoPodePassarComTelefoneNulo() {
-		try {
+	public void contrutorDeBarbeiroNaoPodePassarComTelefoneNulo() 
+	{
+		try 
+		{
 			new Barbeiro("Alessandro", "493.751.185-84", "2258256", null, "10");
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 	}
@@ -167,10 +209,14 @@ public class BarbeiroTeste {
 	 * realização do teste do lançamento da exceção
 	*/
 	@Test (expected = IllegalArgumentException.class)
-	public void contrutorDeBarbeiroNaoPodePassarComCadeiraNulo() {
-		try {
+	public void contrutorDeBarbeiroNaoPodePassarComCadeiraNulo () 
+	{
+		try 
+		{
 			new Barbeiro("Alessandro", "493.751.185-84", "2258256", "3389-9085", null);
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 	}
@@ -180,11 +226,15 @@ public class BarbeiroTeste {
 	 * realização do teste do lançamento da exceção
 	*/
 	@Test (expected = AssertionError.class)
-	public void cpfNaoPodePassarQuandoInvalido() {
-		try {
+	public void cpfNaoPodePassarQuandoInvalido () 
+	{
+		try 
+		{
 			barbeiro.setCpf("000000000");
 			fail();
-		} catch (BarbeiroException e) {
+		}
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 	}
@@ -194,10 +244,14 @@ public class BarbeiroTeste {
 	 * realização do teste do lançamento da exceção
 	*/
 	@Test (expected = AssertionError.class)
-	public void rgNaoPodeConterLetras() {
-		try {
+	public void rgNaoPodeConterLetras () 
+	{
+		try 
+		{
 			barbeiro.setRg("4654654ASD");
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e)
+		{
 			e.printStackTrace();
 		}
 	}
@@ -207,10 +261,14 @@ public class BarbeiroTeste {
 	 * realização do teste do lançamento da exceção
 	*/
 	@Test (expected = AssertionError.class)
-	public void numeroDaCadeiraNaoPodeSerUmaLetra() {
-		try {
+	public void numeroDaCadeiraNaoPodeSerUmaLetra ()
+	{
+		try 
+		{
 			barbeiro.setCadeira("asd");
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e)
+		{
 			e.printStackTrace();
 		}
 	}
@@ -220,10 +278,14 @@ public class BarbeiroTeste {
 	 * realização do teste do lançamento da exceção
 	*/
 	@Test (expected = AssertionError.class)
-	public void numeroDoTelefoneNaoPodeConterLetras() {
-		try {
+	public void numeroDoTelefoneNaoPodeConterLetras () 
+	{
+		try
+		{
 			barbeiro.setTelefone("65465a4");
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 	}
@@ -231,48 +293,60 @@ public class BarbeiroTeste {
 	
 	// Método que testa o recebimento de um nome do Barbeiro pelo método de acesso get 
 	@Test
-	public void testeParaGetterDeNomeDeBarbeiro() {
+	public void testeParaGetterDeNomeDeBarbeiro () 
+	{
 		assertEquals("Alessandro", barbeiro.getNome());
 	}
 	
 	// Método que testa o recebimento de um CPF do Barbeiro pelo método de acesso get 
 	@Test
-	public void testeParaGetterDeCPFDeBarbeiro() {
+	public void testeParaGetterDeCPFDeBarbeiro ()
+	{
 		assertEquals("02919594150", barbeiro.getCpf());
 	}
 	
 	// Método que testa o recebimento de um RG do Barbeiro pelo método de acesso get 
 	@Test
-	public void testeParaGetterDeRGDeBarbeiro() {
+	public void testeParaGetterDeRGDeBarbeiro () 
+	{
 		assertEquals("418757896", barbeiro.getRg());
 	}
 	
 	// Método que testa o recebimento de um telefone do Barbeiro pelo método de acesso get 
 	@Test
-	public void testeParaGetterDeTelefoneDeBarbeiro() {
+	public void testeParaGetterDeTelefoneDeBarbeiro () 
+	{
 		assertEquals("3389-9085", barbeiro.getTelefone());
 	}
 	
 	// Método que testa o recebimento de uma cadeira do Barbeiro pelo método de acesso get 
 	@Test
-	public void testeParaGetterDeCadeiraDeBarbeiro() {
+	public void testeParaGetterDeCadeiraDeBarbeiro ()
+	{
 		assertEquals("10", barbeiro.getCadeira());
 	}
 	
 	// Método que testa o recebimento de um nome temporario do Barbeiro pelo método de acesso get 
 	@Test
-	public void testeParaGetterDeTempNomeDeBarbeiro() {
+	public void testeParaGetterDeTempNomeDeBarbeiro ()
+	{
 		assertEquals(null, Barbeiro.getTempNome());
 	}
 	
 	// Método que testa a passagem de um nome do Barbeiro pelo método de acesso set 
 	@Test
-	public void setDeBarbeiroDeveFuncionar() {
-		try {
+	public void setDeBarbeiroDeveFuncionar ()
+	{
+		try 
+		{
 			barbeiro.setNome("Alessandro");
-		} catch (NullPointerException e) {
+		} 
+		catch (NullPointerException e) 
+		{
 			e.printStackTrace();
-		} catch (BarbeiroException e) {
+		} 
+		catch (BarbeiroException e) 
+		{
 			e.printStackTrace();
 		}
 		assertEquals("Alessandro", barbeiro.getNome());
@@ -280,56 +354,64 @@ public class BarbeiroTeste {
 	
 	// Método que testa a passagem de um nome com número do Barbeiro pelo método de acesso set
 	@Test (expected = BarbeiroException.class)
-	public void nomeComNumero() throws BarbeiroException {
+	public void nomeComNumero () throws BarbeiroException 
+	{
 		barbeiro.setNome("J040");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	// Método que testa a passagem de um CPF em branco do Barbeiro pelo método de acesso set
 	@Test (expected =  BarbeiroException.class)
-	public void cpfPassadoEmBranco() throws BarbeiroException {
+	public void cpfPassadoEmBranco () throws BarbeiroException
+	{
 		barbeiro.setCpf("");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	// Método que testa a passagem de um CPF invalido do Barbeiro pelo método de acesso set
 	@Test (expected =  BarbeiroException.class)
-	public void cpfInvalido() throws BarbeiroException {
+	public void cpfInvalido () throws BarbeiroException 
+	{
 		barbeiro.setCpf("123.654.456-75");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	// Método que testa a passagem de um rg com letras do Barbeiro pelo método de acesso set
 	@Test (expected =  AssertionError.class)
-	public void rgPassadoComLetras() throws BarbeiroException {
+	public void rgPassadoComLetras () throws BarbeiroException 
+	{
 		barbeiro.setRg("asasa");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	// Método que testa a passagem de um rg em branco do Barbeiro pelo método de acesso set
 	@Test (expected =  BarbeiroException.class)
-	public void rgPassadoEmBrancro() throws BarbeiroException {
+	public void rgPassadoEmBrancro () throws BarbeiroException
+	{
 		barbeiro.setRg("");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	// Método que testa a passagem de um nome em branco do Barbeiro pelo método de acesso set
 	@Test (expected =  BarbeiroException.class)
-	public void nomePassadoEmBrancro() throws BarbeiroException {
+	public void nomePassadoEmBrancro () throws BarbeiroException
+	{
 		barbeiro.setNome("");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	// Método que testa a passagem de um telefone em branco do Barbeiro pelo método de acesso set
 	@Test (expected =  BarbeiroException.class)
-	public void telefonePassadoEmBrancro() throws BarbeiroException {
+	public void telefonePassadoEmBrancro () throws BarbeiroException
+	{
 		barbeiro.setTelefone("");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	// Método que testa a passagem de uma cadeira em branco do Barbeiro pelo método de acesso set
 	@Test (expected =  BarbeiroException.class)
-	public void cadeiraPassadoEmBrancro() throws BarbeiroException {
+	public void cadeiraPassadoEmBrancro () throws BarbeiroException 
+	{
 		barbeiro.setCadeira("");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
@@ -339,7 +421,8 @@ public class BarbeiroTeste {
 	 *  pelo método de acesso set
 	 */
 	@Test (expected =  AssertionError.class)
-	public void cadeiraPassadoComoZero() throws BarbeiroException {
+	public void cadeiraPassadoComoZero () throws BarbeiroException 
+	{
 		barbeiro.setCadeira("0");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
@@ -349,20 +432,23 @@ public class BarbeiroTeste {
 	 *   do Barbeiro pelo método de acesso set
 	 */
 	@Test (expected = BarbeiroException.class)
-	public void cadeiraPassadoComMaisDeDoisDigitos() throws BarbeiroException {
+	public void cadeiraPassadoComMaisDeDoisDigitos () throws BarbeiroException
+	{
 		barbeiro.setCadeira("1000");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	// Método que testa o recebimento do nome temporario pelo método de acesso get
 	@Test (expected = AssertionError.class)
-	public void getterDeTempNomeDeveRetornarValorPassado() throws ServicoException {
+	public void getterDeTempNomeDeveRetornarValorPassado () throws ServicoException
+	{
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	 // Método que testa a passagem de um nome temporario nulo 
 	@Test (expected = AssertionFailedError.class)
-	public void setterDeTempNomeNaoPodeSerNulo() throws ServicoException {
+	public void setterDeTempNomeNaoPodeSerNulo () throws ServicoException 
+	{
 		Barbeiro.setTempNome(null);
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
@@ -370,14 +456,16 @@ public class BarbeiroTeste {
 	
 	// Método que testa a passagem de um nome temporario em branco pelo método de acesso set
 	@Test (expected = AssertionFailedError.class)
-	public void setterDeTempNomeNaoPodeSerEmBranco() {
+	public void setterDeTempNomeNaoPodeSerEmBranco () 
+	{
 		Barbeiro.setTempNome("");
 		Assert.fail("Deve lanÃ§ar uma exceÃ§Ã£o");
 	}
 	
 	// Método que testa a passagem de um nome temporário do Barbeiro pelo método de acesso set
 	@Test (expected = AssertionError.class)
-	public void tempNomeValido() throws BarbeiroException {
+	public void tempNomeValido () throws BarbeiroException
+	{
 		Barbeiro.setTempNome("JoÃ£o");
 		assertEquals("JoÃ£o", Agenda.getTempNome());
 	}
