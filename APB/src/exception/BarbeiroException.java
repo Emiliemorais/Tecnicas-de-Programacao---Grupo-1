c9b912a18b3c7c7329f1abea408162dde0fc4619
Italo Paiva <italosensei@hotmail.com>
2014-09-03 12:54:51 -0300

    Aplicação da folha de estilo
    Aplicação nas classes BarbeiroException e ReciboException.

--- APB/src/exception/BarbeiroException.java
+++ APB/src/exception/BarbeiroException.java
@@ -1,15 +1,18 @@
 package exception;
 
 @SuppressWarnings("serial")
-public class BarbeiroException extends Exception {
+public class BarbeiroException extends Exception 
+{
 	
 	// Construtor padrão que referencia o construtor padrão da superclasse
-	public BarbeiroException() {
+	public BarbeiroException()
+	{
 		super();
 	}
 	
 	// Construtor que referencia o construtor da superclasse - Adiciona uma mensagem de exceção
-	public BarbeiroException(String message) {
+	public BarbeiroException(String message)
+	{
 		super(message);
 	}
 
--- APB/src/exception/ReciboException.java
+++ APB/src/exception/ReciboException.java
@@ -1,17 +1,19 @@
 package exception;
 
 @SuppressWarnings("serial")
-public class ReciboException extends Exception {
+public class ReciboException extends Exception 
+{
 
 	// Construtor padrão que referencia o construtor padrão da superclasse
-	public ReciboException() {
+	public ReciboException()
+	{
 		super();
 	}
 	
 	// Construtor que referencia o construtor da superclasse - Adiciona uma mensagem de exceção
-	public ReciboException(String message) {
+	public ReciboException (String message)
+	{
 		super(message);
 	}
 
-	
 }
