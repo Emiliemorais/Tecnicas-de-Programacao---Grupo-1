058148207ea2729d7d85ce4341b2ecc7a21e5479
Italo Paiva <italosensei@hotmail.com>
2014-09-03 11:50:17 -0300

    Aplicação da folha de estilo
    Aplicação na classe RelatorioDAOTeste.

--- APB/src/testes/RelatorioDAOTeste.java
+++ APB/src/testes/RelatorioDAOTeste.java
@@ -1,36 +1,39 @@
 package testes;
 
-import static org.junit.Assert.*;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
 
-import model.Relatorio;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import model.Relatorio;
 import dao.RelatorioDAO;
-
 import exception.RelatorioException;
 
-public class RelatorioDAOTeste {
+public class RelatorioDAOTeste
+{
 
 	Relatorio relatorio = new Relatorio();
 
 	/* 
 	 * Método utilizado para receber os atributos de um relatorio de forma correta,
 	 * para realização do teste, lançando exceçoes de atributos nulos e problemas de conversão
-	*/
+	 */
 	@Before
-	public void setUp() throws RelatorioException, ParseException {
-		try {
+	public void setUp() throws RelatorioException, ParseException
+	{
+		try
+		{
 			relatorio.setBarbeiro("Luciano");
 			relatorio.setDataFinal("09/09/2013");
 			relatorio.setDataInicial("01/01/2013");
 			relatorio.setTipoServico("corte");
-		} catch (NullPointerException e) {
+		} 
+		catch (NullPointerException e)
+		{
 			e.printStackTrace();
 		}
 
@@ -38,23 +41,29 @@ public class RelatorioDAOTeste {
  
 	// Método utilizado para testar o getInstance da classe RelatorioDAO
 	@Test
-	public void getInstanceDeRelatorioDAODeveRetonarInstanciaCorrente() {
+	public void getInstanceDeRelatorioDAODeveRetonarInstanciaCorrente()
+	{
 		RelatorioDAO relatorioDAO = RelatorioDAO.getInstance();
-		assertEquals(RelatorioDAO.getInstance(), relatorioDAO);
+		assertEquals( RelatorioDAO.getInstance(), relatorioDAO );
 	}
 
 	// Método utilizado para testar a vizualização de um relatorio, quando procurado por data
 	@Test
-	public void procurarPorDataDeRelatorioDAODeveMostrarUmRelatorio() {
-		try {
+	public void procurarPorDataDeRelatorioDAODeveMostrarUmRelatorio()
+	{
+		try 
+		{
 			RelatorioDAO relatorioDAO = RelatorioDAO.getInstance();
 			ResultSet rs = relatorioDAO.pesquisarPorData(relatorio);
 			
-			while(rs.next()) {
+			while( rs.next() )
+			{
 				String nome = rs.getString("nome");
 				assertEquals("Corte", nome);
 			}
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
 
@@ -63,18 +72,23 @@ public class RelatorioDAOTeste {
 	/* 
 	 * Método utilizado para testar a vizualização de um relatorio, quando procurado
 	 *  por data e serviço	
-	*/
+	 */
 	@Test
-	public void pesquisarPorDataEServicoDAODeveMostrarUmRelatorio() {
-		try {
+	public void pesquisarPorDataEServicoDAODeveMostrarUmRelatorio()
+	{
+		try 
+		{
 			RelatorioDAO relatorioDAO = RelatorioDAO.getInstance();
 			ResultSet rs = relatorioDAO.pesquisarPorDataEServico(relatorio);
 			
-			while (rs.next()) {
+			while ( rs.next() )
+			{
 				String nome = rs.getString("nome");
 				assertEquals("Corte", nome);
 			}
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
 
@@ -82,16 +96,21 @@ public class RelatorioDAOTeste {
 
 	// Método utilizado para testar a vizualização de um relatorio, quando procurado por barbeiro	
 	@Test
-	public void pesquisarPorBArbeiroDAODeveMostrarUmRelatorio() {
-		try {
+	public void pesquisarPorBArbeiroDAODeveMostrarUmRelatorio()
+	{
+		try
+		{
 			RelatorioDAO relatorioDAO = RelatorioDAO.getInstance();
 			ResultSet rs = relatorioDAO.pesquisarPorBarbeiro(relatorio);
 			
-			while(rs.next()) {
+			while( rs.next() ) 
+			{
 				String nome = rs.getString("nome");
 				assertEquals("Corte", nome);
 			}
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e) 
+		{
 			e.printStackTrace();
 		}
 
@@ -102,16 +121,21 @@ public class RelatorioDAOTeste {
 	 *  por data e serviço	
 	 */
 	@Test
-	public void pesquisarPorBArbeiroEServicoDAODeveMostrarUmRelatorio() {
-		try {
+	public void pesquisarPorBArbeiroEServicoDAODeveMostrarUmRelatorio()
+	{
+		try
+		{
 			RelatorioDAO relatorioDAO = RelatorioDAO.getInstance();
 			ResultSet rs = relatorioDAO.pesquisarPorBarbeiroEServico(relatorio);
 			
-			while(rs.next()) {
+			while( rs.next() ) 
+			{
 				String nome = rs.getString("nome");
 				assertEquals("Corte", nome);
 			}
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
 
@@ -119,16 +143,21 @@ public class RelatorioDAOTeste {
 
 	// Método utilizado para testar a vizualização de um relatorio, quanto procurado serviço	
 	@Test
-	public void pesquisarPorServicoDAODeveMostrarUmRelatorio() {
-		try {
+	public void pesquisarPorServicoDAODeveMostrarUmRelatorio()
+	{
+		try
+		{
 			RelatorioDAO relatorioDAO = RelatorioDAO.getInstance();
 			ResultSet rs = relatorioDAO.pesquisarPorServico(relatorio);
 			
-			while (rs.next()) {
+			while ( rs.next() )
+			{
 				String nome = rs.getString("nome");
 				assertEquals("Corte", nome);
 			}
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
 
@@ -139,16 +168,21 @@ public class RelatorioDAOTeste {
 	 * por data e barbeiro	
 	 */
 	@Test
-	public void pesquisarPorDataEBArbeiroDAODeveMostrarUmRelatorio() {
-		try {
+	public void pesquisarPorDataEBArbeiroDAODeveMostrarUmRelatorio()
+	{
+		try
+		{
 			RelatorioDAO relatorioDAO = RelatorioDAO.getInstance();
 			ResultSet rs = relatorioDAO.pesquisarPorDataEBarbeiro(relatorio);
 			
-			while (rs.next()) {
+			while ( rs.next() ) 
+			{
 				String nome = rs.getString("nome");
 				assertEquals("Corte", nome);
 			}
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e) 
+		{
 			e.printStackTrace();
 		}
 
@@ -159,19 +193,23 @@ public class RelatorioDAOTeste {
 	 * por data, serviço e barbeiro	
 	 */
 	@Test
-	public void pesquisarPorDataBarbeiroEServicoDAODeveMostrarUmRelatorio(){
-		try {
+	public void pesquisarPorDataBarbeiroEServicoDAODeveMostrarUmRelatorio()
+	{
+		try 
+		{
 			RelatorioDAO relatorioDAO = RelatorioDAO.getInstance();
 			ResultSet rs = relatorioDAO.pesquisarPorDataBarbeiroEServico(relatorio);
 			
-			while (rs.next()) {
+			while ( rs.next() ) 
+			{
 				String nome = rs.getString("nome");
 				assertEquals("Corte", nome);
 			}
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
-		
 		
 	}
 
