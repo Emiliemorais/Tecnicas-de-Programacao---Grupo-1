95bee105234e48d0ee0e5e6db31787ac1e67cb31
Leonardo <leoccambraia@yahoo.com.br>
2014-09-03 17:03:52 -0300

    Aplicação da folha de estilo
    Classes:
    - CadastrarServicoPrestado.java
    - BarbeiroController.java

--- APB/src/control/BarbeiroController.java
+++ APB/src/control/BarbeiroController.java
@@ -4,40 +4,61 @@ package control;
 // Importações
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import dao.BarbeiroDAO;
 import model.Barbeiro;
 // Fim das importações
 
 // Início da classe
-public class BarbeiroController {
-
+public class BarbeiroController
+{
 	private static BarbeiroController instance;
 
-	private BarbeiroController() {}
+	private BarbeiroController()
+	{
+	    // Blank
+	}
 
 // Instanciamento
-	public static BarbeiroController getInstance() {
+	public static BarbeiroController getInstance()
+	{
 		if (instance == null)
-			instance = new BarbeiroController();
+        {
+            instance = new BarbeiroController();
+        }
+        else
+        {
+            // Blank
+        }
 		return instance;
 	}
 
 // Método para inserir um barbeiro
-	public boolean inserir(Barbeiro barbeiro) throws SQLException {
+	public boolean inserir(Barbeiro barbeiro) throws SQLException
+	{
 		if (barbeiro == null)
-			return false;
-
+        {
+            return false;
+        }
+        else
+        {
+            // Blank
+        }
 		BarbeiroDAO.getInstance().incluir(barbeiro);
 		return true;
 	}
 // Fim do método
 
 // Método para alterar um barbeiro
-	public boolean alterar(String nome, Barbeiro barbeiro) throws SQLException {
+	public boolean alterar(String nome, Barbeiro barbeiro) throws SQLException
+	{
 		if (barbeiro == null)
-			return false;
-
+        {
+            return false;
+        }
+        else
+        {
+            // Blank
+        }
 		Barbeiro barbeiro_alterado = barbeiro;
 		BarbeiroDAO.getInstance().alterar(nome, barbeiro_alterado, barbeiro);
 		return true;
@@ -45,29 +66,38 @@ public class BarbeiroController {
 // Fim do método
 
 // Método para excluir um barbeiro
-	public boolean excluir(Barbeiro barbeiro) throws SQLException {
+	public boolean excluir(Barbeiro barbeiro) throws SQLException
+	{
 		if (barbeiro == null)
-			return false;
-
+        {
+            return false;
+        }
+        else
+        {
+            // Blank
+        }
 		BarbeiroDAO.getInstance().excluir(barbeiro);
 		return true;
 	}
 // Fim do método
 
 // Interface que prove acesso a pesquisa
-	public ResultSet pesquisar() throws SQLException {
+	public ResultSet pesquisar() throws SQLException
+	{
 		return BarbeiroDAO.getInstance().pesquisar();
 	}
 // Fim da interface que prove acesso a pesquisa	dos barbeiros
 
 // Interface que prove acesso aos barbeiros cadastrados
-	public ResultSet mostrarBarbeirosCadastrados(Barbeiro barbeiro) throws SQLException {
+	public ResultSet mostrarBarbeirosCadastrados(Barbeiro barbeiro) throws SQLException
+	{
 		return BarbeiroDAO.getInstance().mostrarBarbeirosCadastrados(barbeiro);
 	}
 // Fim da interface que prove acesso aos barbeiros cadastrados
 
 // Interface que prove acesso a pesquisa dos barbeiros por nome
-	public ResultSet pesquisarPorNome(Barbeiro barbeiro) throws SQLException {
+	public ResultSet pesquisarPorNome(Barbeiro barbeiro) throws SQLException
+	{
 		return BarbeiroDAO.getInstance().pesquisarPorNome(barbeiro);
 	}
 // Fim da interface que prove acesso a pesquisa dos barbeiros por nome
