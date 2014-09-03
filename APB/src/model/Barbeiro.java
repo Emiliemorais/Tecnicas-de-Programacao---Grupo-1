43b1383704b13db41b8e04e731a2c820e0a54473
Italo Paiva <italosensei@hotmail.com>
2014-09-03 11:20:32 -0300

    Aplicação da folha de estilo
    Aplicação na classe Barbeiro.

--- APB/src/model/Barbeiro.java
+++ APB/src/model/Barbeiro.java
@@ -2,15 +2,8 @@ package model;
 
 import exception.BarbeiroException;
 
-public class Barbeiro {
-
-	private String nome;
-	private String cpf;
-	private String rg;
-	private String telefone;
-	private String cadeira;
-	private static String tempNome;
-	
+public class Barbeiro
+{
 	// Constantes de erros
 	private final String NOME_INVALIDO = "Nome InvÃ¡lido";
 	private final String NOME_BRANCO = "Nome em Branco";
@@ -23,12 +16,23 @@ public class Barbeiro {
 	private final String CADEIRA_INVALIDA = "Cadeira InvÃ¡lida";
 	private final String CADEIRA_BRANCO = "Campo Cadeira em Branco";
 	
-	// Construtor Padrão
-	public Barbeiro() {}
+	private static String tempNome;
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String telefone;
+	private String cadeira;
+		
+		
+	public Barbeiro()
+	{
+		// Construtor Padrão
+	}
 	
 	// Construtor - Testa se os argumentos passados não são nulos
 	public Barbeiro(String nome, String cpf, String rg, String telefone,
-			String cadeira) throws BarbeiroException {
+					String cadeira) throws BarbeiroException
+	{
 		this.nome = nome;
 		this.cpf = cpf;
 		this.rg = rg;
@@ -36,44 +40,80 @@ public class Barbeiro {
 		this.cadeira = cadeira;
 
 		if (this.nome == null)
+		{
 			throw new IllegalArgumentException(NOME_BRANCO);
+		}
+		else
+		{
+			// Nothing to do
+		}
 
 		if (this.cpf == null)
+		{
 			throw new IllegalArgumentException(CPF_BRANCO);
+		}
+		else
+		{
+			// Nothing to do
+		}
 
 		if (this.rg == null)
+		{
 			throw new IllegalArgumentException(RG_BRANCO);
+		}
+		else
+		{
+			// Nothing to do
+		}
 
 		if (this.telefone == null)
+		{
 			throw new IllegalArgumentException(TELEFONE_BRANCO);
+		}
+		else
+		{
+			// Nothing to do
+		}
 
 		if (this.cadeira == null)
+		{
 			throw new IllegalArgumentException(CADEIRA_BRANCO);
+		}
+		else
+		{
+			// Nothing to do
+		}
 	}
 	
 	
 	// Início Getters
-	public String getNome() {
+	public String getNome()
+	{
 		return nome;
 	}
 
-	public String getCpf() {
+	public String getCpf()
+	{
 		return cpf;
 	}
 
-	public String getRg() {
+	public String getRg()
+	{
 		return rg;
 	}
 
-	public String getTelefone() {
+	public String getTelefone()
+	{
 		return telefone;
 	}
 
-	public String getCadeira() {
+	public String getCadeira()
+	{
 		return cadeira;
 	}
 	
-	public static String getTempNome() {
+	public static String getTempNome()
+	{
 		return tempNome;
 	}
 
@@ -83,78 +123,133 @@ public class Barbeiro {
 	// Início Setters
 	
 	// Testa se o nome é válido, e dispara exceções caso contrário
-	public void setNome(String nome) throws BarbeiroException {
+	public void setNome (String nome) throws BarbeiroException 
+	{
 		if (nome == null)
+		{
 			throw new NullPointerException(NOME_BRANCO);
-		else if ("".equals(nome))
+		}
+		else if ( "".equals(nome) )
+		{
 			throw new BarbeiroException(NOME_BRANCO);
-		else if (nome.matches("^[[ ]|\\p{L}*]+$"))
+		}
+		else if ( nome.matches("^[[ ]|\\p{L}*]+$") )
+		{
 			this.nome= nome;
+		}
 		else
+		{
 			throw new BarbeiroException(NOME_INVALIDO);
+		}
 	}
 	
 	// Testa se o cpf é válido, e dispara exceções caso contrário
-	public void setCpf(String cpf) throws BarbeiroException {
+	public void setCpf (String cpf) throws BarbeiroException
+	{
 		// Exemplo CPF vÃ¡lido: 493.751.185-84
-		try {
+		try 
+		{
 			if (cpf == null)
+			{
 				throw new NullPointerException(CPF_BRANCO);
-			else if ("".equals(cpf))
+			}
+			else if ( "".equals(cpf) )
+			{
 				throw new AssertionError(CPF_BRANCO);
-			else if (cpf.matches("[\\d]{3,3}.[\\d]{3,3}.[\\d]{3,3}-[\\d]{2,2}$"))
+			}
+			else if ( cpf.matches("[\\d]{3,3}.[\\d]{3,3}.[\\d]{3,3}-[\\d]{2,2}$") )
+			{
 				cpf = cpf.split("[\\. | -]")[0] + cpf.split("[\\. | -]")[1]
-				+ cpf.split("[\\. | -]")[2] + cpf.split("[\\. | -]")[3];
-			if (this.validarCpf(cpf))
+					  + cpf.split("[\\. | -]")[2] + cpf.split("[\\. | -]")[3];
+			}
+			if ( this.validarCpf(cpf) )
+			{
 				this.cpf = cpf;
+			}
 			else
+			{
 				throw new BarbeiroException(CPF_INVALIDO);
-		} catch (AssertionError e) {
+			}
+		}
+		catch (AssertionError e) 
+		{
 			throw new BarbeiroException(CPF_INVALIDO);
 		}
 	}
 	
 	// Testa se o RG é válido, e dispara exceções caso contrário
-	public void setRg(String rg) throws BarbeiroException {
+	public void setRg (String rg) throws BarbeiroException
+	{
 		if (rg == null)
+		{
 			throw new NullPointerException(RG_BRANCO);
-		else if ("".equals(rg))
+		}
+		else if ( "".equals(rg) )
+		{
 			throw new BarbeiroException(RG_BRANCO);
-		else if (rg.matches("^[[ ]|\\p{L}*]+$"))
+		}
+		else if ( rg.matches("^[[ ]|\\p{L}*]+$") )
+		{
 			throw new AssertionError(RG_INVALIDO);
-		else if (rg.matches("^[0-9]*$"))
+		}
+		else if ( rg.matches("^[0-9]*$") )
+		{
 			this.rg = rg;
+		}
 		else
+		{
 			throw new AssertionError(RG_INVALIDO);
+		}
 	}
 
 	// Testa se o telefone é válido, e dispara exceções caso contrário
-	public void setTelefone(String telefone) throws BarbeiroException {
+	public void setTelefone (String telefone) throws BarbeiroException
+	{
 		if (telefone == null)
+		{
 			throw new NullPointerException(TELEFONE_BRANCO);
-		else if ("".equals(telefone))
+		}
+		else if ( "".equals(telefone) )
+		{
 			throw new BarbeiroException(TELEFONE_BRANCO);
-		else if (telefone.matches("(\\([\\d]{2,3}\\))?[ ]*[\\d]{4,4}[ ]*-[ ]*[\\d]{4,4}[ ]*$"))
+		}
+		else if ( telefone.matches("(\\([\\d]{2,3}\\))?[ ]*[\\d]{4,4}[ ]*-[ ]*[\\d]{4,4}[ ]*$") )
+		{
 			this.telefone = telefone;
+		}
 		else
+		{
 			throw new AssertionError(TELEFONE_INVALIDO);
+		}
 	}
 	
 	// Testa se o argumento cadeira é válido, e dispara exceções caso contrário
-	public void setCadeira(String cadeira) throws BarbeiroException {
+	public void setCadeira (String cadeira) throws BarbeiroException
+	{
 		if (cadeira == null)
+		{
 			throw new NullPointerException(CADEIRA_BRANCO);
+		}
 		else if ("".equals(cadeira))
+		{
 			throw new BarbeiroException(CADEIRA_BRANCO);
-		else if ("0".equals(cadeira) || cadeira.matches("^[[ ]|\\p{L}*]+$"))
+		}
+		else if ( "0".equals(cadeira) || cadeira.matches("^[[ ]|\\p{L}*]+$") )
+		{
 			throw new AssertionError(CADEIRA_INVALIDA);
-		else if (cadeira.matches("^[0-9]{0,2}$"))
+		}
+		else if ( cadeira.matches("^[0-9]{0,2}$") )
+		{
 			this.cadeira = cadeira;
+		}
 		else
+		{
 			throw new BarbeiroException(CADEIRA_INVALIDA);
+		}
 	}
 	
-	public static void setTempNome(String tempNome) {
+	public static void setTempNome (String tempNome)
+	{
 		Barbeiro.tempNome = tempNome;
 	}
 	
@@ -162,16 +257,23 @@ public class Barbeiro {
 	
 		
 	// Método que testa a validade do CPF digitado
-	private boolean validarCpf(String cpf) {
-		int d1, d2;
-		int digito1, digito2, resto;
+	private boolean validarCpf (String cpf) 
+	{
+		int d1 = 0;
+		int d2 = 0;
+		int digito1 = 0;
+		int digito2 = 0;
+		int resto = 0;
 		int digitoCPF;
 		String result;
 
-		d1 = d2 = digito1 = digito2 = resto = 0;
-
-		for (int nCount = 1; nCount < cpf.length() - 1; nCount++) {
-			digitoCPF = Integer.valueOf(cpf.substring(nCount - 1, nCount)).intValue();
+		// d1 = d2 = digito1 = digito2 = resto = 0;
+		
+		int nCount = 1;
+		
+		for (nCount = 1; nCount < cpf.length() - 1; nCount++)
+		{
+			digitoCPF = Integer.valueOf( cpf.substring(nCount - 1, nCount) ).intValue();
 
 			d1 = d1 + (11 - nCount) * digitoCPF;
 			d2 = d2 + (12 - nCount) * digitoCPF;
@@ -180,19 +282,27 @@ public class Barbeiro {
 		resto = d1 % 11;
 
 		if (resto < 2)
+		{
 			digito1 = 0;
+		}
 		else
+		{
 			digito1 = 11 - resto;
+		}
 
 		d2 += 2*digito1;
 		resto = (d2 % 11);
 
 		if (resto < 2)
+		{
 			digito2 = 0;
+		}
 		else
+		{
 			digito2 = 11 - resto;
+		}
 
-		String verific = cpf.substring(cpf.length() - 2, cpf.length());
+		String verific = cpf.substring( cpf.length() - 2, cpf.length() );
 		result = String.valueOf(digito1) + String.valueOf(digito2);
 
 		return verific.equals(result);
