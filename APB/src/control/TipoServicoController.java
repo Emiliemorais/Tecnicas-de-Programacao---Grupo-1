603d1799072cf3ee4e2d8116e46524554ab7d3a8
Leonardo <leoccambraia@yahoo.com.br>
2014-09-03 13:45:55 -0300

    Aplicação da folha de estilo
    Classe "TipoServicoController.java"

--- APB/src/control/TipoServicoController.java
+++ APB/src/control/TipoServicoController.java
@@ -4,21 +4,24 @@ package control;
 // Importando dados
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import dao.TipoServicoDAO;
 import model.TipoServico;
 // Fim da importação dos dados
 
 // Início da classe
-public class TipoServicoController {
-
+public class TipoServicoController
+{
 	private static TipoServicoController instance;
 
 // Método de inserir o tipo de serviço
-	public boolean inserir ( TipoServico tipoServico ) throws SQLException {
-		if ( tipoServico == null ) {
+	public boolean inserir ( TipoServico tipoServico ) throws SQLException
+	{
+		if ( tipoServico == null )
+        {
 			return false;
-		} else {
+		}
+		else
+        {
 			TipoServicoDAO.getInstance ().incluir ( tipoServico );
 			return true;
 		}
@@ -26,10 +29,14 @@ public class TipoServicoController {
 // Fim do método
 
 // Método de alterar o tipo de serviço
-	public boolean alterar ( String nome, TipoServico tipoServico ) throws SQLException {
-		if ( tipoServico == null ) {
+	public boolean alterar ( String nome, TipoServico tipoServico ) throws SQLException
+	{
+		if ( tipoServico == null )
+        {
 			return false;
-		} else {
+		}
+		else
+        {
 			TipoServico tipoServicoAlterado = tipoServico;
 			TipoServicoDAO.getInstance().alterar ( nome, tipoServicoAlterado, tipoServico);
 			return true;
@@ -38,40 +45,53 @@ public class TipoServicoController {
 // Fim do método
 
 // Método de excluir o tipo de serviço
-	public boolean excluir ( TipoServico tipoServico ) throws SQLException {
-
-		if ( tipoServico == null ) {
+	public boolean excluir ( TipoServico tipoServico ) throws SQLException
+	{
+		if ( tipoServico == null )
+        {
 			return false;
-		} else {
+		}
+		else
+        {
 			TipoServicoDAO.getInstance ().excluir ( tipoServico );
 			return true;
 		}
 	}
 // Fim do método
 
-	private TipoServicoController () {
+	private TipoServicoController ()
+	{
+	    // Blank
 	}
 
 // Instanciamento
-	public static TipoServicoController getInstance () {
+	public static TipoServicoController getInstance ()
+	{
 		if ( instance == null )
-			instance = new TipoServicoController ( );
+        {
+            instance = new TipoServicoController ( );
+        }
+        else
+        {
+            // Nothing to do
+        }
 		return instance;
 	}
 // Fim do método
 
 // Interface que prove acesso aos dados dos tipos de serviços cadastrados
-	public ResultSet mostrarTipoServicoCadastrados ( TipoServico servico) throws SQLException {
+	public ResultSet mostrarTipoServicoCadastrados ( TipoServico servico) throws SQLException
+	{
 		return TipoServicoDAO.getInstance().mostrarTipoServicoCadastrados ( servico );
 	}
 // Fim do método
 
 // Interface que prove acesso a pesquisa por nome
-	public ResultSet pesquisarPorNome ( TipoServico servico ) throws SQLException {
+	public ResultSet pesquisarPorNome ( TipoServico servico ) throws SQLException
+	{
 		return TipoServicoDAO.getInstance().pesquisarPorNome ( servico );
 	}
 // Fim do método
 
 }
-
 // Fim da classe
