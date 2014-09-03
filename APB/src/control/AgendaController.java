c309a588dd832af11bb6cfc9d8cc6d6afcf91353
Emiliemorais <emilie.mo@hotmail.com>
2014-09-03 08:43:29 -0300

    Aplicação da folha de estilo

--- APB/src/control/AgendaController.java
+++ APB/src/control/AgendaController.java
@@ -7,58 +7,94 @@ import dao.AgendaDAO;
 import model.Agenda;
 
 
-public class AgendaController {
+public class AgendaController 
+{
 	
 	private static AgendaController instance;
 	
-	private AgendaController() {}
-	
-        // Método que recebe como parâmetro o objeto agenda e inclui no banco de dados
-	public boolean incluir(Agenda agenda) throws SQLException {
-		if(agenda == null )
-			return false;
+	private AgendaController() 
+	{
 		
-		AgendaDAO.getInstance().incluir(agenda);
-		return true;
 	}
 	
-        // Método que recebe como parâmetro o nome do contato a ser altera e o objeto agenda e altera no banco de dados
-	public boolean alterar(String nome,Agenda agenda) throws SQLException {
+    // Método que recebe como parâmetro o objeto agenda e inclui no banco de dados
+	public boolean incluir (Agenda agenda) throws SQLException 
+	{
 		if(agenda == null )
+		{
 			return false;
+		}
 		
-		Agenda agenda_alterado = agenda;
-		AgendaDAO.getInstance().alterar(nome ,agenda_alterado, agenda);
-		return true;		
+		// Se o objeto agenda não for nulo o método inclui no banco de dados
+		else{
+			AgendaDAO.getInstance().incluir(agenda);
+			return true;
+		}
+	}
+	
+    // Método que recebe como parâmetro o nome do contato a ser altera e o objeto agenda e altera no banco de dados
+	public boolean alterar (String nome, Agenda agenda) throws SQLException 
+	{
+		if(agenda == null)
+		{
+			return false;
+		}
+		
+		// Se o objeto agenda não for nulo o método altera no banco de dados
+		else{
+			Agenda agenda_alterado = agenda;
+			AgendaDAO.getInstance().alterar(nome, agenda_alterado, agenda);
+			return true;		
+		}
 	}
         
         // Método que recebe como parâmetro o objeto agenda e exclui do banco de dados
-	public  boolean excluir(Agenda contato) throws SQLException {
-		if (contato == null)
+	public  boolean excluir (Agenda contato) throws SQLException 
+	{
+		if(contato == null)
+		{
 			return false;
+		}
 		
-		AgendaDAO.getInstance().excluir(contato);
-		return true;
+
+		// Se o objeto agenda não for nulo o método exclui no banco de dados
+		else
+		{
+			AgendaDAO.getInstance().excluir(contato);
+			return true;
+		}
+
 	}
 	
-	public static AgendaController getInstance() {
+	// Método que pega a instância de uma Agenda
+	public static AgendaController getInstance () 
+	{
 		if(instance == null)
+		{
 			instance = new AgendaController();
+		}
+		else
+		{
+			// Nothing to do
+		}
 		return instance;
 	}
 	
         // Interface que provê acesso aos contatos cadastrados e mostra todos os contatos
-	public ResultSet mostrarContatosCadastrados(Agenda contato) throws SQLException {
+	public ResultSet mostrarContatosCadastrados(Agenda contato) throws SQLException 
+	{
 		return AgendaDAO.getInstance().mostrarContatosCadastrados(contato);
 	}
 	
         // Interface que provê acesso aos contatos cadastrados e permite que sejam pesquisados pelo nome
-	public ResultSet pesquisarPorNome(Agenda contato) throws SQLException {
+	public ResultSet pesquisarPorNome(Agenda contato) throws SQLException 
+	{
 		return AgendaDAO.getInstance().pesquisarPorNome(contato);
 	}
 	
         // Interface que provê acesso aos contatos cadastrados e permite que sejam pesquisados pelo telefone
-	public ResultSet pesquisarPorTelefone(Agenda contato) throws SQLException {
+	public ResultSet pesquisarPorTelefone(Agenda contato) throws SQLException 
+	{
 		return AgendaDAO.getInstance().pesquisarPorTelefone(contato);
 	}
 
--- APB/src/dao/RelatorioDAO.java
+++ APB/src/dao/RelatorioDAO.java
@@ -7,21 +7,33 @@ import java.sql.SQLException;
 
 import model.Relatorio;
 
-public class RelatorioDAO {
+public class RelatorioDAO 
+{
 
 	private static RelatorioDAO instance;
 	
-	private RelatorioDAO(){}
+	private RelatorioDAO()
+	{
+		
+	}
 
-	public static RelatorioDAO getInstance() {
+	// Método que pega a instância de um relatório
+	public static RelatorioDAO getInstance ()
+	{
 		if (instance == null)
+		{
 			instance = new RelatorioDAO();
+		}
+		else
+		{
+			
+		}
 		return instance;
 	}
 
-        // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por data
-	public ResultSet pesquisarPorData(Relatorio relatorio)
-			throws SQLException {
+    // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por data
+	public ResultSet pesquisarPorData (Relatorio relatorio) throws SQLException
+	{
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		PreparedStatement pst = connection.prepareStatement("SELECT * FROM servicoprestado WHERE data BETWEEN '"
 				+relatorio.getDataInicial()+"' AND '"+relatorio.getDataFinal()+"';");
@@ -30,9 +42,9 @@ public class RelatorioDAO {
 		return rs;
 	}
         
-        // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por data e por barbeiro 
-	public ResultSet pesquisarPorDataEBarbeiro(Relatorio relatorio)
-			throws SQLException {
+    // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por data e por barbeiro 
+	public ResultSet pesquisarPorDataEBarbeiro(Relatorio relatorio) throws SQLException 
+	{
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		PreparedStatement pst = connection.prepareStatement("SELECT * FROM servicoprestado WHERE data BETWEEN '"
 				+relatorio.getDataInicial()+"' AND '"+relatorio.getDataFinal()+"' AND barbeiro = '"
@@ -42,9 +54,9 @@ public class RelatorioDAO {
 		return rs;
 	}
         
-        // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por data e serviço
-	public ResultSet pesquisarPorDataEServico(Relatorio relatorio)
-			throws SQLException {
+    // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por data e serviço
+	public ResultSet pesquisarPorDataEServico(Relatorio relatorio) throws SQLException
+	{
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		PreparedStatement pst = connection.prepareStatement("SELECT * FROM servicoprestado WHERE data BETWEEN '"
 				+relatorio.getDataInicial()+"' AND '"+relatorio.getDataFinal()+"' AND nome = '"
@@ -52,12 +64,11 @@ public class RelatorioDAO {
 		ResultSet rs = pst.executeQuery();
 		
 		return rs;
-	
 	}
         
-        // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por barbeiro
-	public ResultSet pesquisarPorBarbeiro(Relatorio relatorio)
-			throws SQLException {
+    // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por barbeiro
+	public ResultSet pesquisarPorBarbeiro(Relatorio relatorio) throws SQLException
+	{
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		PreparedStatement pst = connection.prepareStatement("SELECT * FROM servicoprestado WHERE barbeiro = '"
 				+relatorio.getBarbeiro()+"';");
@@ -66,9 +77,9 @@ public class RelatorioDAO {
 		return rs;
 	}
         
-        // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por barbeiro e servico
-	public ResultSet pesquisarPorBarbeiroEServico(Relatorio relatorio)
-			throws SQLException {
+    // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por barbeiro e servico
+	public ResultSet pesquisarPorBarbeiroEServico(Relatorio relatorio) throws SQLException 
+	{
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		PreparedStatement pst = connection.prepareStatement("SELECT * FROM servicoprestado WHERE barbeiro = '"
 				+relatorio.getBarbeiro()+"' AND nome = '"+relatorio.getTipoServico()+"';");
@@ -77,9 +88,9 @@ public class RelatorioDAO {
 		return rs;
 	}
         
-        // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por servico
-	public ResultSet pesquisarPorServico(Relatorio relatorio)
-			throws SQLException {
+    // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por servico
+	public ResultSet pesquisarPorServico(Relatorio relatorio)throws SQLException 
+	{
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		PreparedStatement pst = connection.prepareStatement("SELECT * FROM servicoprestado WHERE nome = '"
 				+relatorio.getTipoServico()+"';");
@@ -88,9 +99,9 @@ public class RelatorioDAO {
 		return rs;
 	}
         
-        // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por data, barbeiro e serviço
-	public ResultSet pesquisarPorDataBarbeiroEServico(Relatorio relatorio)
-			throws SQLException {
+    // Interface que provê acesso aos relatorios cadastrados e permite que sejam pesquisados por data, barbeiro e serviço
+	public ResultSet pesquisarPorDataBarbeiroEServico(Relatorio relatorio)throws SQLException
+	{
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		PreparedStatement pst = connection.prepareStatement("SELECT * FROM servicoprestado WHERE data BETWEEN '"
 				+relatorio.getDataInicial()+"' AND '"+relatorio.getDataFinal()+"' AND barbeiro = '"
