5ba7fbfcc6a1251613ce4f9a422bcc509ba72764
Leonardo <leoccambraia@yahoo.com.br>
2014-09-13 15:58:49 -0300

    Aplicação da folha de estilos
    Classe "ServicoPrestadoController.java"

--- APB/src/control/ServicoPrestadoController.java
+++ APB/src/control/ServicoPrestadoController.java
@@ -1,44 +1,79 @@
+//Pacote ao qual a classe pertence
 package control;
 
+// Importações
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import dao.ServicoPrestadoDAO;
 import model.ServicoPrestado;
+// Fim das importações
 
-public class ServicoPrestadoController {
+// Inicio da classe
+public class ServicoPrestadoController
+{
 
-	private ServicoPrestadoController() {}
-	
-	public static ServicoPrestadoController getInstance() {
+	private ServicoPrestadoController()
+	{
+	    // Blank
+	}
+
+	// Instanciamento
+	public static ServicoPrestadoController getInstance()
+	{
 		if (instance == null)
-			instance = new ServicoPrestadoController();
-		return instance;
+        {
+            instance = new ServicoPrestadoController();
+        }
+        else
+        {
+            // Nothing to do
+        }
+
+        return instance;
 	}
 
 	private static ServicoPrestadoController instance;
 
-	public boolean inserir(ServicoPrestado servico) throws SQLException {
-		if (servico != null) {
+    // Metodo que insere um servico
+	public boolean inserir(ServicoPrestado servico) throws SQLException
+	{
+		if (servico != null)
+        {
 			ServicoPrestadoDAO.getInstance().incluir(servico);
 			return true;
 		}
-		
+		else
+        {
+            // Nothing to do
+        }
+
 		return false;
 	}
+    // Fim do metodo
 
-	public boolean excluir(ServicoPrestado servico) throws SQLException {
-		if (servico !=  null) {
+    // Metodo que exclui um servico
+	public boolean excluir(ServicoPrestado servico) throws SQLException
+	{
+		if (servico !=  null)
+        {
 			ServicoPrestadoDAO.getInstance().excluir(servico);
 			return true;
-
 		}
-		return false;	
+		else
+        {
+            // Nothing to do
+        }
+		return false;
 	}
-	
-	public ResultSet mostrarServicosPrestadosCadastrados(ServicoPrestado servico) throws SQLException {
+	// Fim do metodo
+
+	// Metodo que prove informações sobre os servico prestados que estao cadastrados
+	public ResultSet mostrarServicosPrestadosCadastrados(ServicoPrestado servico) throws SQLException
+	{
 		return ServicoPrestadoDAO.getInstance().mostrarServicosPrestadosCadastrados(servico);
 	}
+    // Fim do metodo
 
-	
 }
+// Fim da classe
