f9a61e65b8a7fa2219ad1b64969d40a1436643e6
Emilie Morais <emilie.mo@hotmail.com>
2014-10-20 21:21:20 -0200

    Activity 5 - Atomizing methods

--- APB/src/view/APB.java
+++ APB/src/view/APB.java
@@ -1,4 +1,3 @@
-
 package view;
 
 import java.awt.EventQueue;
@@ -52,28 +51,64 @@ public class APB extends JFrame
 	 */
 	public APB()
 	{
+		// These methods are used to initialize the components  
+		initializeFrame();
+		initializePanel();
+		initializeLabels();
+		initializeButtons();
+
+	}
+
+	// This method is used to initialize the main frame of the application
+	private void initializeFrame() 
+	{
 		setTitle("APB");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 583, 357);
+		setBounds(100, 100, 583, 357);				
+	}
+	
+	// This method is used to initialize the panel to insert the components
+	private void initializePanel() 
+	{
 		contentPane = new JPanel();
 		contentPane.setForeground( new Color(255, 255, 255) );
 		contentPane.setBorder( new EmptyBorder(5, 5, 5, 5) );
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblWelcome = new JLabel("Bem Vindo");
-		lblWelcome.setFont( new Font("Tahoma", Font.PLAIN, 17) );
-		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
-		lblWelcome.setBounds(195, 13, 160, 38);
-		contentPane.add(lblWelcome);
 
-		JLabel lblAPB = new JLabel("Automa\u00E7\u00E3o de Processos da Barbearia");
-		lblAPB.setFont(new Font("Tahoma", Font.PLAIN, 15) );
-		lblAPB.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAPB.setBounds(127, 175, 288, 57);
-		contentPane.add(lblAPB);
+	}
+
+	// This method is used to initialize the buttons
+	private void initializeButtons() 
+	{
+
+		createButtonToStart();
+		createButtonToClose();
+
+	}
+	
+	// This method is used to create the button and the action that close the application
+	private void createButtonToClose() 
+	{
+		JButton btnClose = new JButton("Fechar");
+		btnClose.addActionListener(new ActionListener()
+        {
+			public void actionPerformed(ActionEvent e)
+            {
+				System.exit(0);
+			}
+		}
+		);
+		btnClose.setIcon(new ImageIcon
+						(APB.class.getResource("/resources/cancel-button-icone-7221-48.png") ) );
+		btnClose.setBounds(338, 242, 145, 65);
+		contentPane.add(btnClose);
 		
-		// Button that starts the application
+	}
+	
+	// This method is used to create the button and the action that starts the application
+	private void createButtonToStart() 
+	{
 		JButton btnStart = new JButton("Iniciar");
 		btnStart.addActionListener(new ActionListener()
         {
@@ -91,25 +126,31 @@ public class APB extends JFrame
 		btnStart.setBounds(82, 242, 137, 65);
 		contentPane.add(btnStart);
 		
-		// Button that close the application
-		JButton btnClose = new JButton("Fechar");
-		btnClose.addActionListener(new ActionListener()
-        {
-			public void actionPerformed(ActionEvent e)
-            {
-				System.exit(0);
-			}
-		}
-		);
-		btnClose.setIcon(new ImageIcon
-						(APB.class.getResource("/resources/cancel-button-icone-7221-48.png") ) );
-		btnClose.setBounds(338, 242, 145, 65);
-		contentPane.add(btnClose);
+	}
+
+	// This method is used to initialize the labels of the frame
+	private void initializeLabels() 
+	{
+		// Label to welcome
+		JLabel lblWelcome = new JLabel("Bem Vindo");
+		lblWelcome.setFont( new Font("Tahoma", Font.PLAIN, 17) );
+		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcome.setBounds(195, 13, 160, 38);
+		contentPane.add(lblWelcome);
+
+		// Label to describe the 'Barbearia Peixoto'
+		JLabel lblAPB = new JLabel("Automa\u00E7\u00E3o de Processos da Barbearia");
+		lblAPB.setFont(new Font("Tahoma", Font.PLAIN, 15) );
+		lblAPB.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAPB.setBounds(127, 175, 288, 57);
+		contentPane.add(lblAPB);		
+			
 		
 		// Object that refers to the icon of 'Barbearia Peixoto' logo.
 		JLabel lblIcon = new JLabel("");
 		lblIcon.setIcon(new ImageIcon
-					   (APB.class.getResource("/resources/19fb55_f0fe0bafb3f2bec53376838a10e0825a.jpg_srz_401_146_75_22_0.50_1.20_0.00_jpg_srz.jpg") ) );
+					   (APB.class.getResource("/resources/19fb55_f0fe0bafb3f2bec53376838a10e0825a."
+					   						  + "jpg_srz_401_146_75_22_0.50_1.20_0.00_jpg_srz.jpg")) );
 		lblIcon.setBounds(82, 62, 401, 119);
 		contentPane.add(lblIcon);
 	}
--- APB/src/view/Administrative.java
+++ APB/src/view/Administrative.java
@@ -1,14 +1,19 @@
 
 package view;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.JButton;
@@ -51,13 +56,26 @@ public class Administrative extends JFrame
 	 */
 	public Administrative()
 	{
+		// These methods are used to initialize the components  
+		initializeFrame();
+		initializePanel();
+		
+	}
+	// This method is used to initialize the main frame of the application
+	private void initializeFrame() 
+	{
 		setTitle("APB");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 418, 238);
+		setBounds(100, 100, 418, 238);			
+	}
+	
+	// This method is used to initialize the panel to insert the components
+	private void initializePanel() 
+	{
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5) );
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(null);	
 		
 		// Panel that contains the content of Administrative panel
 		JPanel administrativePanel = new JPanel();
@@ -68,6 +86,53 @@ public class Administrative extends JFrame
 		contentPane.add(administrativePanel);
 		administrativePanel.setLayout(null);
 		
+		// This method initialize the buttons
+		initializeButtons(administrativePanel);
+		
+	}
+
+	// This method is used to initialize the buttons
+	private void initializeButtons(JPanel administrativePanel) 
+	{
+				
+		createButtonToBackTheMainFrame(administrativePanel);				
+		createButtonToOpenBarberFrame(administrativePanel);
+		createButtonToOpenPhonebookFrame(administrativePanel);
+		createButtonToOpenReceiptFrame(administrativePanel);
+		createButtonToOpenServiceTypeFrame(administrativePanel);
+
+	}
+	
+	// This method is used to create the button and the action that back to the main frame
+		private void createButtonToBackTheMainFrame(JPanel administrativePanel) 
+		{
+			
+			/* 
+			 * Button that calls the class 'MainMenu' 
+			 * (Create a frame that links to the main menu)
+			 */
+			JButton btnBack = new JButton("Voltar");
+			btnBack.addActionListener(new ActionListener()
+	        {
+				public void actionPerformed(ActionEvent arg0)
+	            {
+					dispose();
+					
+					// Frame used to "go back" to the main menu
+					MainMenu mainMenuFrame = new MainMenu();
+					mainMenuFrame.setVisible(true);
+					mainMenuFrame.setLocationRelativeTo(null);
+				}
+			}
+			);
+			btnBack.setBounds(151, 141, 89, 23);
+			administrativePanel.add(btnBack);
+		}
+		
+	
+	// This method is used to create the button and the action that open the barber frame
+	private void createButtonToOpenBarberFrame(JPanel administrativePanel) 
+	{
 		// Button that calls the class 'RegisterBarber' (Create a frame to register a barber)
 		JButton btnBarber = new JButton("Barbeiro");
 		btnBarber.addMouseListener(new MouseAdapter()
@@ -85,28 +150,12 @@ public class Administrative extends JFrame
 		}
 		);
 		btnBarber.setBounds(10, 45, 157, 37);
-		administrativePanel.add(btnBarber);
-		
-		/* 
-		 * Button that calls the class 'RegisterServiceType'
-		 *  (Create a frame to register a service type)
-		 */
-		JButton btnServiceType = new JButton("Tipo de Servi\u00E7o");
-		btnServiceType.addActionListener(new ActionListener()
-        {
-			public void actionPerformed(ActionEvent e)
-            {
-				dispose();
-				
-				// Frame used to register a serviceType
-				RegisterServiceType registerServiceTypeFrame = new RegisterServiceType();
-				registerServiceTypeFrame.setVisible(true);
-				registerServiceTypeFrame.setLocationRelativeTo(null);
-			}
-		});
-		btnServiceType.setBounds(215, 45, 149, 37);
-		administrativePanel.add(btnServiceType);
-		
+		administrativePanel.add(btnBarber);	
+	}
+	
+	// This method is used to create the button and the action that open the Phonebook frame
+	private void createButtonToOpenPhonebookFrame(JPanel administrativePanel) 
+	{
 		/* 
 		 * Button that calls the class 'RegisterPhonebook'
 		 * (Create a frame to register a phonebook)
@@ -128,28 +177,12 @@ public class Administrative extends JFrame
 		);
 		btnPhonebook.setBounds(10, 93, 157, 37);
 		administrativePanel.add(btnPhonebook);
-		
-		/* 
-		 * Button that calls the class 'MainMenu' 
-		 * (Create a frame that links to the main menu)
-		 */
-		JButton btnBack = new JButton("Voltar");
-		btnBack.addActionListener(new ActionListener()
-        {
-			public void actionPerformed(ActionEvent arg0)
-            {
-				dispose();
-				
-				// Frame used to "go back" to the main menu
-				MainMenu mainMenuFrame = new MainMenu();
-				mainMenuFrame.setVisible(true);
-				mainMenuFrame.setLocationRelativeTo(null);
-			}
-		}
-		);
-		btnBack.setBounds(151, 141, 89, 23);
-		administrativePanel.add(btnBack);
-		
+	
+	}
+	
+	// This method is used to create the button and the action that open the receipt frame
+	private void createButtonToOpenReceiptFrame(JPanel administrativePanel) 
+	{
 		/*
 		 * Button that calls the class 'GenerateReceipt' 
 		 * (Create a frame that allows to create a receipt)
@@ -179,8 +212,33 @@ public class Administrative extends JFrame
 		}
 		);
 		btnReceipt.setBounds(215, 93, 149, 37);
-		administrativePanel.add(btnReceipt);
+		administrativePanel.add(btnReceipt);	
 	}
 
+	// This method is used to create the button and the action that open the Service Type frame
+		private void createButtonToOpenServiceTypeFrame(JPanel administrativePanel) 
+		{
+			
+			/* 
+			 * Button that calls the class 'RegisterServiceType'
+			 *  (Create a frame to register a service type)
+			 */
+			JButton btnServiceType = new JButton("Tipo de Servi\u00E7o");
+			btnServiceType.addActionListener(new ActionListener()
+	        {
+				public void actionPerformed(ActionEvent e)
+	            {
+					dispose();
+					
+					// Frame used to register a serviceType
+					RegisterServiceType registerServiceTypeFrame = new RegisterServiceType();
+					registerServiceTypeFrame.setVisible(true);
+					registerServiceTypeFrame.setLocationRelativeTo(null);
+				}
+			});
+			btnServiceType.setBounds(215, 45, 149, 37);
+			administrativePanel.add(btnServiceType);
+			
+		}
 }
 
