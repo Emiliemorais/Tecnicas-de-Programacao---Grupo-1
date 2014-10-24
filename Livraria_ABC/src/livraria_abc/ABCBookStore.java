afa9783c32be39ff7ff073602fb7ef57b35867bb
thyago241 <thiagoribeironiquel@hotmail.com>
2014-10-24 11:03:11 -0200

    Activity 5 - Atomizing methods
    ABCBookStore

--- Livraria_ABC/src/livraria_abc/ABCBookStore.java
+++ Livraria_ABC/src/livraria_abc/ABCBookStore.java
@@ -37,30 +37,87 @@ public class ABCBookStore
         System.out.println("\nObrigado por utilizar nossos serviços! A Livraria ABC agrade e volte sempre!");
     }
     
-    public static void showMenu ()
-    {
-        // Receives the option from the user about what to do in the system
+    public static String showOptionMenu(){
         String option = "";
         
-        // Checks if someone has logged.
-        if( currentAccess )
+        if(!option.equals("0"))
         {
-            if(!option.equals("0"))
-            {
-                System.out.println("\nOlá "+currentClientName+"! O que deseja na Livraria Virtual ABC?");
-                System.out.println("1 - Olhar catálogo");
-                System.out.println("2 - Procurar por Livro específico");
-                System.out.println("3 - Adicionar livro à cesta");
-                System.out.println("4 - Visualizar cesta");
-                System.out.println("5 - Efetuar logout");
-                System.out.println("6 - Retirar item da cesta");
-                System.out.println("7 - Incluir cupom de desconto");
-                System.out.println("8 - Fechar cesta e finalizar compra");
-                System.out.println("9 - Acessar sua conta");
-                System.out.println("0 - Efetuar logout, fechar página e sair do sistema");
+            System.out.println("\nOlá "+currentClientName+"! O que deseja na Livraria Virtual ABC?");
+            System.out.println("1 - Olhar catálogo");
+            System.out.println("2 - Procurar por Livro específico");
+            System.out.println("3 - Adicionar livro à cesta");
+            System.out.println("4 - Visualizar cesta");
+            System.out.println("5 - Efetuar logout");
+            System.out.println("6 - Retirar item da cesta");
+            System.out.println("7 - Incluir cupom de desconto");
+            System.out.println("8 - Fechar cesta e finalizar compra");
+            System.out.println("9 - Acessar sua conta");
+            System.out.println("0 - Efetuar logout, fechar página e sair do sistema");
             
-                option = inputString.nextLine();
+            option = inputString.nextLine();
+        }else{
+            //Noting to do
+        }
+        return option;
+    }
+    
+    public static void finishCart(){
+        // Receives the code of the buying
+        int buyingCode;
+
+        buyingCode = Cart.closeCart();
+
+        if( buyingCode == -1 )
+        {
+            showMenu();
+        }
+        else
+        {
+            buyingCode = buyingCode;
+            Purchase.checkAddress();
+        }
+        //// Passar esse codigo para classe compra. Se cod_compra = -1 ta errado. VALIDAR!!!
+
+    }
+    public static void addBookCart(){
+        String researchedBookByUser;
+                    
+        System.out.println("\nInforme o ISBN ou o título do livro que deseja: ");
+        researchedBookByUser = inputString.nextLine();
+
+        if( Book.listISBN.contains(researchedBookByUser) || Book.listTitle.contains(researchedBookByUser) )
+        {
+            // Receives the index of the book researched by user
+            int indexOfTheBook = -1;
+
+            // Receives the quantity of books 
+            int bookQuantity;
+
+            if( Book.listISBN.contains(researchedBookByUser) )
+            {
+                indexOfTheBook = Book.listISBN.indexOf(researchedBookByUser);
             }
+            else if( Book.listTitle.contains(researchedBookByUser) )
+            {
+                indexOfTheBook = Book.listTitle.indexOf(researchedBookByUser);
+            }
+
+            System.out.println("\nInforme a quantidade de '"+Book.listTitle.get(indexOfTheBook)+"' desejada:");
+            bookQuantity = input.nextInt();
+
+            Cart.addItemsToCart(Book.listISBN.get(indexOfTheBook), bookQuantity, currentClient);
+        }
+        else
+        {
+            String messageFailResearched = "\nNão encontramos '"+researchedBookByUser+"' em nosso sistema."
+                                         + " Verifique a grafia e tente novamente.\n";
+            System.out.println(messageFailResearched);
+            showMenu();
+        }
+    }
+    
+    public static void showMenuForLogger(String option){
+        option = showOptionMenu();
             
             // Book class's instance to access the class
             Book book = new Book();
@@ -83,40 +140,7 @@ public class ABCBookStore
                 case "3":
                 {
                     // Receives the listISBN or the title of the book researched by user
-                    String researchedBookByUser;
-                    
-                    System.out.println("\nInforme o ISBN ou o título do livro que deseja: ");
-                    researchedBookByUser = inputString.nextLine();
-                    
-                    if( Book.listISBN.contains(researchedBookByUser) || Book.listTitle.contains(researchedBookByUser) )
-                    {
-                        // Receives the index of the book researched by user
-                        int indexOfTheBook = -1;
-                        
-                        // Receives the quantity of books 
-                        int bookQuantity;
-                        
-                        if( Book.listISBN.contains(researchedBookByUser) )
-                        {
-                            indexOfTheBook = Book.listISBN.indexOf(researchedBookByUser);
-                        }
-                        else if( Book.listTitle.contains(researchedBookByUser) )
-                        {
-                            indexOfTheBook = Book.listTitle.indexOf(researchedBookByUser);
-                        }
-                        
-                        System.out.println("\nInforme a quantidade de '"+Book.listTitle.get(indexOfTheBook)+"' desejada:");
-                        bookQuantity = input.nextInt();
-                        
-                        Cart.addItemsToCart(Book.listISBN.get(indexOfTheBook), bookQuantity, currentClient);
-                    }
-                    else
-                    {
-                        String messageFailResearched = "\nNão encontramos '"+researchedBookByUser+"' em nosso sistema."
-                                                     + " Verifique a grafia e tente novamente.\n";
-                        System.out.println(messageFailResearched);
-                        showMenu();
-                    }
+                    addBookCart();
                     
                     break;
                 }
@@ -142,22 +166,7 @@ public class ABCBookStore
                 }
                 case "8":
                 {
-                    // Receives the code of the buying
-                    int buyingCode;
-                    
-                    buyingCode = Cart.closeCart();
-                    
-                    if( buyingCode == -1 )
-                    {
-                        showMenu();
-                    }
-                    else
-                    {
-                        buyingCode = buyingCode;
-                        Purchase.checkAddress();
-                    }
-                    //// Passar esse codigo para classe compra. Se cod_compra = -1 ta errado. VALIDAR!!!
-                    
+                    finishCart();
                     break;
                 }
                 case "9":
@@ -180,67 +189,82 @@ public class ABCBookStore
                    
                 }
             }
+    }
+    public static void showMenuForNotLogger( String option){
+        if( !option.equals("0") )
+        {            
+            System.out.println("\nOlá visitante! O que deseja na Livraria Virtual ABC?");
+            System.out.println("1 - Olhar catálogo");
+            System.out.println("2 - Procurar por Livro específico");
+            System.out.println("3 - Adicionar livro à cesta");
+            System.out.println("4 - Efetuar_login");
+            System.out.println("0 - Fechar página e sair do sistema");
+
+            option = inputString.nextLine();
+        }else{
+            //nothing to do
+        }
+
+
+        // Book class's instance to access the class
+        Book book = new Book();
+
+        // Client class's instance to access the class
+        Client client = new Client();
+
+        switch(option)
+        {
+            case "1":
+            {
+                book.showCatalog();
+                break;
+            }
+            case "2":
+            {
+                book.queryBook();
+                break;
+            }
+            case "3":
+            {
+                System.out.println("\nÉ necessário fazer login primeiro.");
+
+                client.getLogin();
+
+                showMenu();
+                break;
+            }
+            case "4":
+            {
+                client.getLogin();
+                break;
+            }
+            case "0":
+            {
+                showExitMessage();
+
+               // break;
+            }
+            default:
+            {
+                if( !"0".equals(option) ){
+                    System.out.println("\nOpção Inválida!\n");
+                }
+            }
+        }
+    }
+    public static void showMenu ()
+    {
+        // Receives the option from the user about what to do in the system
+        String option = "";
+        
+        // Checks if someone has logged.
+        if( currentAccess )
+        {
+            showMenuForLogger(option);
         }
         else
         {
-            if( !option.equals("0") )
-            {            
-                System.out.println("\nOlá visitante! O que deseja na Livraria Virtual ABC?");
-                System.out.println("1 - Olhar catálogo");
-                System.out.println("2 - Procurar por Livro específico");
-                System.out.println("3 - Adicionar livro à cesta");
-                System.out.println("4 - Efetuar_login");
-                System.out.println("0 - Fechar página e sair do sistema");
             
-                option = inputString.nextLine();
-            }
-            
-               
-            // Book class's instance to access the class
-            Book book = new Book();
-            
-            // Client class's instance to access the class
-            Client client = new Client();
-            
-            switch(option)
-            {
-                case "1":
-                {
-                    book.showCatalog();
-                    break;
-                }
-                case "2":
-                {
-                    book.queryBook();
-                    break;
-                }
-                case "3":
-                {
-                    System.out.println("\nÉ necessário fazer login primeiro.");
-                    
-                    client.getLogin();
-                    
-                    showMenu();
-                    break;
-                }
-                case "4":
-                {
-                    client.getLogin();
-                    break;
-                }
-                case "0":
-                {
-                    showExitMessage();
-                       
-                   // break;
-                }
-                default:
-                {
-                    if( !"0".equals(option) )
-                        System.out.println("\nOpção Inválida!\n");
-                    
-                }
-            }
         }
     }
     
